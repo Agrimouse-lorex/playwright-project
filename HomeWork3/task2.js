d15644a226836a856ca4e29bc5b4015d7ed8dd49
Tromsa Oleksandr <agrimouse@gmail.com>
2025-09-04 14:05:57 +0300

    lesson4

--- HomeWork3/task2.js
+++ HomeWork3/task2.js
@@ -4,17 +4,20 @@ console.log(age)
 function getRandom(min, max) {
     return Math.floor(Math.random()*(max-min+1))+min;
 }
+
+
 age = getRandom(10, 30)
 console.log("Age:", age);
+
 let isAdult;
 function adultCheck(age) {
 /*    if (age >= 18) {
-        return isAdult = true;
+        return isAdult = true; // My own idea of how to write that function
     } else {
         return isAdult = false;
     }
 */
-    isAdult = age >= 18;
+    isAdult = age >= 18; // simplifying my function
     return isAdult;
 }
 console.log(adultCheck(age))
---
+++ HomeWork4/task2.js
@@ -0,0 +1,9 @@
+import getRandom from '../HomeWork3/random.js';
+
+let hour = getRandom(0,23)
+console.log("Hour:", hour);
+
+if(hour >= 0 && hour <= 5) {console.log("Good night!")}
+else if(hour >= 6 && hour <= 11) {console.log("Good Morning!")}
+else if(hour >= 12 && hour <= 17) {console.log("Good day!")}
+else if(hour >= 18 && hour <= 23) {console.log("Good evening!")};
